67ab5d11cebbc5a069143e8fe785d02cc235cf10
Cătălin Dogaru <catalin.dogaru@gmail.com>
2016-04-23 22:04:46 +0300

    Updated d3 code

--- source/public/controllers/SchematicsController.js
+++ source/public/controllers/SchematicsController.js
@@ -19,113 +19,10 @@ module.exports = function ()
     window.Wyliodrin = {};
   }
 
-  (function (Wyliodrin) {
-    Wyliodrin.bc = { // Breadboard Circuits
-      parts: [],
-      libraryPath: '/public/drawable/parts'
-    };
-  })(Wyliodrin);
-
-  var canvasId = 'canvas';
-  var canvasWidth = '100%';
-  var canvasHeight = '100%';
-
-  var gridId = 'grid';
-  var gridSize = 80;
-  var gridFill = 'none';
-  var gridStroke = '#ddd';
-  var gridStrokeWidth = 1;
-
-  var zoomed = function () {
-    pattern.attr('patternTransform', 'translate(' + d3.event.translate + ') scale(' + d3.event.scale + ')');
-    defs.select('path').attr('stroke-width', 0.5 / d3.event.scale);
-    pattern.select('path').attr('stroke-width', 1 / d3.event.scale);
-    group.attr('transform', 'translate(' + d3.event.translate + ') scale(' + d3.event.scale + ')');
+  Wyliodrin.bc = { // Breadboard Circuits
+    parts: [],
+    libraryPath: '/public/drawable/parts'
   };
-
-  var zoom = d3.behavior.zoom()
-    .scaleExtent([0.5, 4])
-    .on('zoom', zoomed);
-
-  var dragstarted = function () {
-    d3.event.sourceEvent.stopPropagation();
-    d3.select(this).classed('dragging', true);
-  };
-
-  var dragged = function () {
-    d3.select(this)
-      .attr('x', d3.event.x)
-      .attr('y', d3.event.y);
-  };
-
-  var dragended = function () {
-    d3.select(this).classed('dragging', false);
-  };
-
-  d3.behavior.drag()
-    .origin(function () {
-      return {
-        x: d3.select(this).attr('x'),
-        y: d3.select(this).attr('y')
-      };
-    })
-    .on('dragstart', dragstarted)
-    .on('drag', dragged)
-    .on('dragend', dragended);
-
-  var canvas = d3.select('#viewport')
-    .append('svg:svg')
-    .attr('id', canvasId)
-    .attr('width', canvasWidth)
-    .attr('height', canvasHeight)
-    .call(zoom)
-    .on('mousedown', function () {
-      canvas.style('cursor', 'grabbing');
-    })
-    .on('mouseup', function () {
-      canvas.style('cursor', 'grab');
-    })
-    .style('cursor', 'grab');
-
-  var defs = canvas.append('defs');
-
-  var pattern = defs.append('pattern')
-    .attr('id', 'grid-inner')
-    .attr('width', 8)
-    .attr('height', 8)
-    .attr('patternUnits', 'userSpaceOnUse');
-
-  pattern.append('path')
-    .attr('d', 'M ' + '8' + ' 0 L 0 0 0 ' + '8')
-    .attr('fill', gridFill)
-    .attr('stroke', '#eee')
-    .attr('stroke-width', 0.5);
-
-  pattern = defs.append('pattern')
-    .attr('id', gridId)
-    .attr('width', gridSize)
-    .attr('height', gridSize)
-    .attr('patternUnits', 'userSpaceOnUse');
-
-  pattern.append('rect')
-    .attr('fill', 'url(#grid-inner)')
-    .attr('width', gridSize)
-    .attr('height', gridSize);
-
-  pattern.append('path')
-    .attr('d', 'M ' + gridSize + ' 0 L 0 0 0 ' + gridSize)
-    .attr('fill', gridFill)
-    .attr('stroke', gridStroke)
-    .attr('stroke-width', gridStrokeWidth);
-
-  var grid = canvas.append('rect')
-    .attr('width', canvasWidth)
-    .attr('height', canvasHeight)
-    .attr('fill', 'url(#' + gridId + ')');
-
-  grid.visible = true;
-
-  var group = canvas.append('g');
 
   function loadParts($http, $rootScope, $scope) {
     $http.get(Wyliodrin.bc.libraryPath + '/parts.json', {
@@ -200,6 +97,107 @@ module.exports = function ()
     {
       schematics: ''
     };
+
+    var canvasId = 'canvas';
+    var canvasWidth = '100%';
+    var canvasHeight = '100%';
+
+    var gridId = 'grid';
+    var gridSize = 80;
+    var gridFill = 'none';
+    var gridStroke = '#ddd';
+    var gridStrokeWidth = 1;
+
+    var zoomed = function () {
+      pattern.attr('patternTransform', 'translate(' + d3.event.translate + ') scale(' + d3.event.scale + ')');
+      defs.select('path').attr('stroke-width', 0.5 / d3.event.scale);
+      pattern.select('path').attr('stroke-width', 1 / d3.event.scale);
+      group.attr('transform', 'translate(' + d3.event.translate + ') scale(' + d3.event.scale + ')');
+    };
+
+    var zoom = d3.behavior.zoom()
+      .scaleExtent([0.5, 4])
+      .on('zoom', zoomed);
+
+    var dragstarted = function () {
+      d3.event.sourceEvent.stopPropagation();
+      d3.select(this).classed('dragging', true);
+    };
+
+    var dragged = function () {
+      d3.select(this)
+        .attr('x', d3.event.x)
+        .attr('y', d3.event.y);
+    };
+
+    var dragended = function () {
+      d3.select(this).classed('dragging', false);
+    };
+
+    d3.behavior.drag()
+      .origin(function () {
+        return {
+          x: d3.select(this).attr('x'),
+          y: d3.select(this).attr('y')
+        };
+      })
+      .on('dragstart', dragstarted)
+      .on('drag', dragged)
+      .on('dragend', dragended);
+
+    var canvas = d3.select('#viewport')
+      .append('svg:svg')
+      .attr('id', canvasId)
+      .attr('width', canvasWidth)
+      .attr('height', canvasHeight)
+      .call(zoom)
+      .on('mousedown', function () {
+        canvas.style('cursor', 'grabbing');
+      })
+      .on('mouseup', function () {
+        canvas.style('cursor', 'grab');
+      })
+      .style('cursor', 'grab');
+
+    var defs = canvas.append('defs');
+
+    var pattern = defs.append('pattern')
+      .attr('id', 'grid-inner')
+      .attr('width', 8)
+      .attr('height', 8)
+      .attr('patternUnits', 'userSpaceOnUse');
+
+    pattern.append('path')
+      .attr('d', 'M ' + '8' + ' 0 L 0 0 0 ' + '8')
+      .attr('fill', gridFill)
+      .attr('stroke', '#eee')
+      .attr('stroke-width', 0.5);
+
+    pattern = defs.append('pattern')
+      .attr('id', gridId)
+      .attr('width', gridSize)
+      .attr('height', gridSize)
+      .attr('patternUnits', 'userSpaceOnUse');
+
+    pattern.append('rect')
+      .attr('fill', 'url(#grid-inner)')
+      .attr('width', gridSize)
+      .attr('height', gridSize);
+
+    pattern.append('path')
+      .attr('d', 'M ' + gridSize + ' 0 L 0 0 0 ' + gridSize)
+      .attr('fill', gridFill)
+      .attr('stroke', gridStroke)
+      .attr('stroke-width', gridStrokeWidth);
+
+    var grid = canvas.append('rect')
+      .attr('width', canvasWidth)
+      .attr('height', canvasHeight)
+      .attr('fill', 'url(#' + gridId + ')');
+
+    grid.visible = true;
+
+    var group = canvas.append('g');
 
     loadParts($http, $rootScope, $scope);
 
